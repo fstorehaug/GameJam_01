438f79c18ac0e4d50d391b7c5564fa6265c107d0
Bjørn <bmvalbergiversen@gmail.com>
2021-08-14 18:45:29 +0200

    add scripts for updating death counter and victory counter

---
+++ Assets/DeathCounter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathCounter : MonoBehaviour
+{
+    [SerializeField]
+    private Text deathCounterText;
+    private int numDeaths;
+
+    public void updateNumDeaths()
+    {
+        numDeaths++;
+        deathCounterText.text = "Deaths: " + numDeaths.ToString();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        deathCounterText.text = "Deaths: 0";
+        // TODO: Add subscription to "Death" event
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
